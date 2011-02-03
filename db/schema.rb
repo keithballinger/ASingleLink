2452ce356adc60b80e8bb4809f5a7715b89ada03
Keith Ballinger <keithba@gmail.com>
2011-02-02 19:59:02 -0500

    added facebookid to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110203005025) do
+ActiveRecord::Schema.define(:version => 20110203005746) do
 
   create_table "users", :force => true do |t|
     t.string   "first_name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20110203005025) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "facebook_id"
   end
 
 end
