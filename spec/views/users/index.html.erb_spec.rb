93e0c13f62bdc26558e15058bf141e88275f0313
Keith Ballinger <keithba@gmail.com>
2011-02-02 19:54:54 -0500

    initial migration of User

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "users/index.html.erb" do
+  before(:each) do
+    assign(:users, [
+      stub_model(User,
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :email => "Email"
+      ),
+      stub_model(User,
+        :first_name => "First Name",
+        :last_name => "Last Name",
+        :email => "Email"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "First Name".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Last Name".to_s, :count => 2
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+  end
+end
