83ddc522922a0a59f7b9b1fb083cd612f523a1c1
Keith Ballinger <keithba@gmail.com>
2011-02-07 20:12:39 -0500

    placed user id in session and put common header HTML in application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :current_user
+  
+  protected 
+  
+  def current_user
+    user_id = session[:current_user_id]
+    
+    @user = User.find_by_id user_id
+    return @user
+  end
+  
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,15 +8,17 @@ class SessionsController < ApplicationController
       last_name = auth['user_info']['last_name'] 
       email = auth['extra']['user_hash']['email'] 
     
-      @user = User.find_by_facebook_id(facebook_id)
+      user = User.find_by_facebook_id(facebook_id)
       
-      if @user.nil?
-        @user = User.create :facebook_id => facebook_id, 
+      if user.nil?
+        user = User.create :facebook_id => facebook_id, 
                             :first_name => first_name,
                             :last_name => last_name, 
                             :email => email
       end
       
-      redirect_to '/users/'
+      session[:current_user_id] = user.id
+      
+      redirect_to '/'
   end
 end
